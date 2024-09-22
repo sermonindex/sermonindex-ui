bf4c281c8fde4f8905c3a69dfcb2c35945278834
rotareti <>
2024-09-22 14:24:22 -0600

    Initial Commit
    This is a boilerplate commit that uses the `npx create-remix@latest`
    command. After pulling in the remix boilerplate I cut out all the
    "tutorial" artifacts and added a simple Header component that simply
    has the sermon-index.svg icon. Lastly, I added a very hack "SI"
    favicon.

---
+++ app/components/header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return (
+        <div className="fixed top-0 left-0 right-0 bg-si-main p-[1.5rem]">
+            <div className="w-1/4">
+                <img
+                    src="/sermon-index.svg"
+                    alt="sermon-index"
+                />
+            </div>
+        </div>
+    );
+}
