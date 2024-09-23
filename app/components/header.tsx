2e82ff1fa1adf2839a5d6d51dd09d4f476734587
rotareti <>
2024-09-22 22:43:53 -0600

    Make Header be fixed height
    Working with dynamic sized styling gets tricky (for me)
    with server rendered components. I think we could change
    this in the future if we wanted, but for now this was a
    simple approach.

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,11 @@
 export const Header = () => {
     return (
-        <div className="fixed top-0 left-0 right-0 bg-si-main p-[1.5rem]">
-            <div className="w-1/4">
-                <img
-                    src="/sermon-index.svg"
-                    alt="sermon-index"
-                />
-            </div>
+        <div className="flex fixed top-0 left-0 right-0 h-40 bg-si-main p-[1.5rem] items-center justify-left">
+            <img
+                className="h-full object-contain"
+                src="/sermon-index.svg"
+                alt="sermon-index"
+            />
         </div>
     );
 }
