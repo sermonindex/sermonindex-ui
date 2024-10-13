bab22e5a40844662668ab340d16288aa284120d8
rotareti <>
2024-10-13 07:42:02 -0600

    Revert custom screen sizes
    If we want to define custom breakpoints for screen sizes, we need to
    define the `smallAudioLayoutQuery` hook for the vid stack media player.
    That would be easy enough to do, but I'm wondering if we really want
    custom screen sizes.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,15 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  screens: {
-    sm: "640px",
-    md: "768px",
-    lg: "1024px",
-    xl: "1280px",
-    "2xl": "1536px",
-  },
+  // If we want to define custom breakpoints for screen sizes, we need to
+  // define the `smallAudioLayoutQuery` hook for the vid stack media player
+  // screens: {
+  //   sm: "640px",
+  //   md: "768px",
+  //   lg: "1024px",
+  //   xl: "1280px",
+  //   "2xl": "1536px",
+  // },
   theme: {
     extend: {
       fontFamily: {
