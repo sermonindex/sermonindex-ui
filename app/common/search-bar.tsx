e0ca2f69d90d8e52b7a2ad64c79c6b61e5a93ebd
rotareti <>
2024-11-03 15:16:39 -0600

    Add generic search bar component and use in header

---
+++ app/common/search-bar.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+interface SearchProps {
+  placeholder: string;
+  // Either on change or on submit should be defined
+  onChange?: any; // todo: redefine as callable
+  onSubmit?: any; // todo: redefine as callable
+}
+
+export const SearchBar: React.FC<SearchProps> = ({
+  placeholder,
+  onChange,
+  onSubmit,
+}) => {
+  return (
+    <div className="relative flex flex-grow">
+      <input
+        className="flex-grow bg-gray-50 text-gray-900 text-sm rounded-lg px-4 py-2 pr-10"
+        placeholder={placeholder}
+        onChange={(e) => {
+          /* todo */
+        }}
+        required
+      />
+      {/* todo: only show submit button if onSubmit is defined */}
+      <button
+        type="submit"
+        className="absolute top-1/2 right-3 transform -translate-y-1/2 bg-transparent border-none cursor-pointer"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 text-gray-400"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,4 +1,5 @@
 import { Link, useLocation } from '@remix-run/react';
+import { SearchBar } from '~/common/search-bar';
 
 const Pages = ['speakers', 'audio', 'video', 'text', 'about'];
 const Shortcuts = ['bible', 'hymns', 'books', 'pictures'];
@@ -40,41 +41,14 @@ export const Header = () => {
         <Link to="/">
           <div className="flex-shrink-0">
             <img
-              className="h-20 min-w-full py-3 pr-8 object-contain"
+              className="h-24 min-w-full py-3 pr-8 object-contain"
               src="/sermon-index.svg"
               alt="sermon-index"
             />
           </div>
         </Link>
-        <div className="flex flex-grow pl-10 pr-20">
-          {/* Todo: Bust out this search bar into a shared component */}
-          <div className="relative flex flex-grow">
-            <input
-              className="flex-grow bg-gray-50 text-gray-900 text-sm rounded-lg px-4 py-2 pr-10"
-              placeholder="Search SermonIndex..."
-              onChange={(e) => {
-                /* todo */
-              }}
-              required
-            />
-            <button
-              type="submit"
-              className="absolute top-1/2 right-3 transform -translate-y-1/2 bg-transparent border-none cursor-pointer"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-gray-400"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+        <div className="flex flex-grow pl-8 pr-20">
+          <SearchBar placeholder="Search Sermons..." />
         </div>
       </div>
       {/* Second navbar - page links */}
