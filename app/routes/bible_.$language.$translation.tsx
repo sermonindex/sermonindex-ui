bee64aa37a88d554aa56d96abd0d2ff193c68215
rndoam <rndoam.random@gmail.com>
2025-04-04 09:36:11 -0600

    feat: replace bible-list with generic list

--- app/routes/bible_.$language.$translation.tsx
+++ app/routes/bible_.$language.$translation.tsx
@@ -1,10 +1,11 @@
-import SiPage from '~/components/si-page';
-import { SiSection } from '~/components/section';
 import { LoaderFunctionArgs } from '@remix-run/node';
-import { fetchApi } from '~/api/sdk';
 import { useLoaderData } from '@remix-run/react';
-import { BibleTranslation } from '~/api/interfaces';
-import { BookList } from '~/components/book-list';
+import { BibleBook, BibleTranslation } from '~/api/interfaces';
+import { fetchApi } from '~/api/sdk';
+import { isOldTestament } from '~/common/get-bible-book-id.fn';
+import { GenericList } from '~/components/generic-list';
+import { SiSection } from '~/components/section';
+import SiPage from '~/components/si-page';
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const { _, language, translation } = params;
@@ -21,6 +22,17 @@ export async function loader({ params }: LoaderFunctionArgs) {
   return bible;
 }
 
+const getBibleBookGroupedItems = (books: BibleBook[]) => {
+  return books.reduce((grouped, book) => {
+    const canon = isOldTestament(book.name) ? 'Old Testament' : 'New Testament';
+    if (!grouped[canon]) {
+      grouped[canon] = [];
+    }
+    grouped[canon].push(book);
+    return grouped;
+  }, {} as { [key: string]: BibleBook[] });
+};
+
 export default function Index() {
   const bible = useLoaderData<typeof loader>();
 
@@ -32,7 +44,18 @@ export default function Index() {
             This translation does not include the full biblical canon.
           </p>
         )}
-        <BookList books={bible.books} translation={bible.shortName} />
+        <GenericList<BibleBook>
+          items={bible.books}
+          getGroupedItems={getBibleBookGroupedItems}
+          getGroupKeyName={(key: string) => key}
+          getItemId={(book: BibleBook) => book.id}
+          getItemName={(book: BibleBook) => book.name}
+          getItemLink={(book: BibleBook) =>
+            `/bible/${bible.shortName}/${book.id}/1`
+          }
+          getItemCount={(book: BibleBook) => book.numberOfChapters}
+          columnsClassName="grid-cols-1 md:grid-cols-2"
+        />
       </SiSection>
     </SiPage>
   );
