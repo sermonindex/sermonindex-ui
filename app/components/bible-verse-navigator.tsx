56378b0264ec671bc2d364cbd91befcf9a145ecc
rotareti <rotareti+github@gmail.com>
2025-08-30 21:52:09 -0600

    Cleanup previous wip commit (#14)
    I had planned on cleaning up the previous wip commit, but
    it was prematurely merged. Since I prefer a linear history
    over a dirty wip commit in the main branch, this is the
    cleanup commit for the mess I left in wip.
    
    Co-authored-by: rotareti <sermonindex@gmail.com>

--- app/components/bible-verse-navigator.tsx
+++ app/components/bible-verse-navigator.tsx
@@ -1,61 +1,62 @@
 import { Link } from '@remix-run/react';
 import { IconContext } from 'react-icons';
 import {
-    FaRegArrowAltCircleLeft,
-    FaRegArrowAltCircleRight,
+  FaRegArrowAltCircleLeft,
+  FaRegArrowAltCircleRight,
 } from 'react-icons/fa';
 import { OsisToBookName } from '~/common/bible-constants';
 
 export interface VerseNavigatorProps {
-    book: string;
-    chapter: number;
-    verse: number;
-    previousUrl: string;
-    nextUrl: string;
-    showPrevious: boolean;
-    showNext: boolean;
+  book: string;
+  chapter: number;
+  verse: number;
+  previousUrl: string;
+  nextUrl: string;
+  showPrevious: boolean;
+  showNext: boolean;
 }
 
 export function VerseNavigator({
-                                   book,
-                                   chapter,
-                                   verse,
-                                   previousUrl,
-                                   nextUrl,
-                                   showPrevious,
-                                   showNext,
-                               }: VerseNavigatorProps) {
-    const iconContextValue = {
-        className: 'w-6 h-6 md:w-7 md:h-7 text-neutral-600 dark:text-neutral-200 hover:text-si-main dark:hover:text-si-brown',
-    };
+  book,
+  chapter,
+  verse,
+  previousUrl,
+  nextUrl,
+  showPrevious,
+  showNext,
+}: VerseNavigatorProps) {
+  const iconContextValue = {
+    className:
+      'w-6 h-6 md:w-7 md:h-7 text-neutral-600 dark:text-neutral-200 hover:text-si-main dark:hover:text-si-brown',
+  };
 
-    return (
-        <div className="flex w-full min-h-16 items-center justify-center gap-x-8 md:gap-x-12 mb-4">
-            {showPrevious ? (
-                <Link to={previousUrl} prefetch="intent" title="Previous Verse">
-                    <IconContext.Provider value={iconContextValue}>
-                        <FaRegArrowAltCircleLeft />
-                    </IconContext.Provider>
-                </Link>
-            ) : (
-                // Add a placeholder to maintain balance when one arrow is hidden
-                <div className="w-6 md:w-7" />
-            )}
+  return (
+    <div className="flex w-full min-h-16 items-center justify-center gap-x-8 md:gap-x-12 mb-4">
+      {showPrevious ? (
+        <Link to={previousUrl} prefetch="intent" title="Previous Verse">
+          <IconContext.Provider value={iconContextValue}>
+            <FaRegArrowAltCircleLeft />
+          </IconContext.Provider>
+        </Link>
+      ) : (
+        // Add a placeholder to maintain balance when one arrow is hidden
+        <div className="w-6 md:w-7" />
+      )}
 
-            <h1 className="text-2xl md:text-3xl text-center whitespace-nowrap">
-                {OsisToBookName[book as keyof typeof OsisToBookName]} {chapter}:{verse}
-            </h1>
+      <h1 className="text-2xl md:text-3xl text-center whitespace-nowrap">
+        {OsisToBookName[book as keyof typeof OsisToBookName]} {chapter}:{verse}
+      </h1>
 
-            {showNext ? (
-                <Link to={nextUrl} prefetch="intent" title="Next Verse">
-                    <IconContext.Provider value={iconContextValue}>
-                        <FaRegArrowAltCircleRight />
-                    </IconContext.Provider>
-                </Link>
-            ) : (
-                // Add a placeholder to maintain balance when one arrow is hidden
-                <div className="w-6 md:w-7" />
-            )}
-        </div>
-    );
+      {showNext ? (
+        <Link to={nextUrl} prefetch="intent" title="Next Verse">
+          <IconContext.Provider value={iconContextValue}>
+            <FaRegArrowAltCircleRight />
+          </IconContext.Provider>
+        </Link>
+      ) : (
+        // Add a placeholder to maintain balance when one arrow is hidden
+        <div className="w-6 md:w-7" />
+      )}
+    </div>
+  );
 }
